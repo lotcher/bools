69d4091463bdb7dc4f63055ec12fa2609b0ca7da
bowaer <cb229435444@outlook.com>
2021-05-27 14:27:16 +0800

    fix: setup readme必须是相对路径

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from os import path
 from setuptools import find_packages
 
 BASE_DIR = path.dirname(path.abspath(__file__))
-README_DIR = path.join(BASE_DIR, 'README.md')
+README_DIR = 'README.md'
 with open(path.join(BASE_DIR, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.read().split()
 
 setup(
     name='bools',
-    version='0.1.0',
+    version='0.1.1',
     description='常用工具库',
     author='bowaer',
     author_email='cb229435444@outlook.com',
@@ -24,5 +24,4 @@ setup(
     long_description=open(README_DIR, encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     data_files=[README_DIR]
-
 )
